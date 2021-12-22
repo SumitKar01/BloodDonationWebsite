49a95f8f0b68f8448e322c46505f7bb5fc387bab
Sumit Kumar Kar <sumit.kar@northsouth.edu>
2021-12-22 14:34:44 +0600

    Login Functionality Added

--- account.php
+++ account.php
@@ -1,22 +1,11 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "blood_donation");
-if(!$db){
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$sql = "SELECT nid FROM users WHERE username = '$username' AND password = '$password'";
-
-$result = mysqli_query($db, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    echo "Login Successful" . $row['nid'];
+session_start();
+if($_SESSION['loggedin'] != true){
+    header("Location: 404.html");
 }
 else {
-    echo "Login Failed";
+    echo "Welcome ".$_SESSION['nid'];
+    session_destroy();
 }
 
-
+?>
---
+++ login.php
@@ -0,0 +1,102 @@
+<?php
+session_start();
+$falsepass=false;
+if(isset($_POST['submit'])){
+    $db = mysqli_connect("localhost", "root", "", "blood_donation");
+    if(!$db){
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT nid FROM users WHERE username = '$username' AND password = '$password'";
+
+    $result = mysqli_query($db, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['nid'] = $row['nid'];
+        $_SESSION['loggedin'] = true;
+        header("Location: account.php");
+        
+    }
+    else{
+        $falsepass= true;
+    }
+}
+?>
+    
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="title">
+                <h1>
+                    Rokto
+                </h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.html">Register</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section id="login-area">
+        <div class="container">
+            <h1>Login</h1>
+            <form action="login.php" method="post">
+                <div id="login-box">
+                    <div id="username-box">
+                        <label for="username">Username</label>
+                        <br>
+                        <input type="text" name="username" id="username" placeholder="Username/Email/Phone">
+                    </div>
+                    <div id="password-box">
+                        <label for="password">Password</label>
+                        <br>
+                        <input type="password" name="password" id="password" placeholder="Password">
+                    </div>
+                    <div id="forgot">
+                        <p>
+                        <?php
+                            if($falsepass==true){
+                                echo "Wrong username or password";
+                            }
+                        ?>
+                        </p>
+                        <a href="">Forgot Password </a>
+                    </div>
+                    <div id="login-button">
+                        <input type="submit" name="submit" value="Login">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <section>
+                <h3>Contact Info:</h3>
+                <p>Email: abcd@gmailcom</p>
+            </section>
+        </div>
+    </footer>
+</body>
+
+</html>
+
